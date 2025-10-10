b08ed108d002e61b5cd9b56701c6282aff269f72
marzwu <marzwu@gmail.com>
2025-10-10 16:29:09 +0800

    ! update version

--- binance/__init__.py
+++ binance/__init__.py
@@ -4,7 +4,7 @@
 
 """
 
-__version__ = "1.0.19.17"
+__version__ = "1.0.19.18"
 
 from binance.client import Client, AsyncClient  # noqa
 from binance.depthcache import DepthCacheManager, OptionsDepthCacheManager, ThreadedDepthCacheManager  # noqa
